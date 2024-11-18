bb55e72e5d7631c4328ac351722e9e8a52d6ca9e
Roman Ghazi <rog001@edu.zealand.dk>
2024-11-18 12:57:09 +0100

    Rettet fejl

--- MusicRecordREST/Models/MusicRecord.cs
+++ MusicRecordREST/Models/MusicRecord.cs
@@ -2,5 +2,19 @@
 {
     public class MusicRecord
     {
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+        public void ValidateYear()
+        {
+            if (PublicationYear == 0)
+            {
+
+            }
+
+        }
     }
 }
--- MusicRecordREST/Models/Repository.cs
+++ MusicRecordREST/Models/Repository.cs
@@ -2,20 +2,7 @@
 {
     public class Repository
     {
-        public string? Title { get; set; }
-        public string? Artist { get; set; }
-        public int Duration { get; set; }
-        public int PublicationYear { get; set; }
-
-
-        public void ValidateYear()
-        {
-            if (PublicationYear == 0)
-            {
-
-            }
-
-        }
+       
     }
 }
 
