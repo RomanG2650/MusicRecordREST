f4acb26db124f12ab44a30fce930b3831f153b67
Jou0001 <jout0001@edu.zealand.dk>
2024-11-20 10:06:49 +0100

    Lavet GetAll testen i musicControllerTest

---
+++ MusicRecordRESTTests/Controllers/MusicRecordsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicRecordREST.Controllers;
+using MusicRecordREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicRecordREST.Controllers.Tests
+{
+    [TestClass()]
+    public class MusicRecordsControllerTests
+    {
+        [TestMethod()]  
+        public void GetTest()
+        {
+            MusicRecordRepositoryList recordsRepositoryList = new MusicRecordRepositoryList();
+
+           var list = recordsRepositoryList.GetAll();
+
+            Assert.AreEqual(7, list.Count);
+
+
+
+        }
+    }
+}
